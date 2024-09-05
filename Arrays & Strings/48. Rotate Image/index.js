b439c8fd48e8ef72dec8b824983a0ac260657695
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-09-05 13:21:28 +0330

    Add solution to the Rotate Image problem

---
+++ Arrays & Strings/48. Rotate Image/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+var rotate = function(matrix) {
+    const len = matrix.length;
+
+    for (let i=0; i<len; i++) {
+        for (let j=i+1; j<len; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    for (let i=0; i<len; i++) {
+        for (let j=0; j<len / 2; j++) {
+            [matrix[i][j], matrix[i][len - j - 1]] = [matrix[i][len - j - 1], matrix[i][j]];
+        }
+    }
+};
