b6d11b9e26576135a03f52aef228b054ff7200af
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-09-09 14:51:35 +0330

    Add the solution of the problem

---
+++ Hashmaps & Sets/771. Jewels and Stones/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+ 
+var numJewelsInStones = function(jewels, stones) {
+    const set = new Set(jewels);
+
+    let count = 0;
+
+    for (let stone of stones) {
+        if (set.has(stone)) {
+            count++;
+        }
+    }
+
+    return count;
+};
