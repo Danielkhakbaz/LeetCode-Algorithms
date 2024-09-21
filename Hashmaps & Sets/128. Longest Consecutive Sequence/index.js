179fa6fd17a4e568028feef558cf64a5824e123d
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-09-21 15:02:02 +0330

    Add the solution of the problem

---
+++ Hashmaps & Sets/128. Longest Consecutive Sequence/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var longestConsecutive = function(nums) {
+    const set = new Set(nums);
+
+    let longest = 0;
+
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+            let length = 1;
+            let nextNum = num + 1;
+
+            while (set.has(nextNum)) {
+                length++;
+                nextNum++;
+            }
+
+            longest = Math.max(longest, length);
+        }
+    }
+
+    return longest;
+};
