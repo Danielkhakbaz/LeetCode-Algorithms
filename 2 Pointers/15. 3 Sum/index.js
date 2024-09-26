4d434d5c9c3c9066ce9a2439a748bd1316a8c2a5
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-09-26 15:00:32 +0330

    Add the solution to the problem

---
+++ 2 Pointers/15. 3 Sum/index.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var threeSum = function(nums) {
+    const sortedNums = nums.sort((a, b) => a - b);
+
+    const result = [];
+
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] > 0) {
+            break;
+        }
+        if (nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let low = i + 1, high = nums.length - 1;
+
+        while (low < high) {
+            let sum = nums[i] + nums[low] + nums[high];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[low], nums[high]]);
+                
+                low++;
+                high--;
+
+                while (nums[low] === nums[low - 1]) low++;
+                while (nums[low] === nums[high + 1]) high--;
+            } else if (sum > 0) {
+                high--;
+            } else {
+                low++;
+            }
+        }
+    }
+
+    return result;
+};
