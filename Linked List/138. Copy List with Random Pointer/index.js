edf834452d073af028a56ecb2434132cfdbbc06b
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-10-19 12:14:30 +0330

    Add solution to the problem

---
+++ Linked List/138. Copy List with Random Pointer/index.js
@@ -0,0 +1,35 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {_Node} head
+ * @return {_Node}
+ */
+var copyRandomList = function(head) {
+    if (!head) return null;
+
+    const oldToNew = new Map();
+    let curr = head;
+
+    while (curr) {
+        oldToNew.set(curr, new Node(curr.val));
+        curr = curr.next;
+    }
+
+    curr = head;
+
+    while (curr) {
+        const newNode = oldToNew.get(curr);
+        newNode.next = oldToNew.get(curr.next) || null;
+        newNode.random = oldToNew.get(curr.random) || null;
+        curr = curr.next;
+    }
+
+    return oldToNew.get(head);
+};
