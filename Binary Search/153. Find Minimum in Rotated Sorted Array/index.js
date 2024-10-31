d160c98f42610795f0c63ea8a990a6e42e582e7c
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-10-31 16:22:21 +0330

    Add the solution to the problem

--- Binary Search/153. Find Minimum in Rotated Sorted Array/index.js
+++ Binary Search/153. Find Minimum in Rotated Sorted Array/index.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 
-var findMin = function(nums) {
+var findMin = function (nums) {
     let left = 0;
     let right = nums.length - 1;
 
-    while ( right) {
+    while (left < right) {
         let mid = Math.floor((left + right) / 2);
 
         if (nums[mid] > nums[right]) {
             left = mid + 1;
         } else {
-            right = mid - 1;
+            right = mid;
         }
     }
 
