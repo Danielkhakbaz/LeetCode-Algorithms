289ef807b133186d6a24b94556acfa2da87e26cf
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-12 12:04:58 +0330

    Add a new solution to the problem

--- 2 Pointers/344. Reverse String/index.js
+++ 2 Pointers/344. Reverse String/index.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 
-var reverseString = function(s) {
+var reverseString = function (s) {
     // s.reverse();
 
     // Time C: O(n^2)
@@ -20,4 +20,13 @@ var reverseString = function(s) {
 
     // Time C: O(n)
     // Space C: O(1)
+
+    // let left = 0, right = s.length - 1;
+
+    // for (let i=0; i<(s.length - 1) / 2; i++) {
+    //     [s[left], s[right]] = [s[right], s[left]];
+
+    //     left++;
+    //     right--;
+    // }
 };
