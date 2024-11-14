737c0db3e3004297d37bd87f722080ddb5b69d3f
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-14 14:58:29 +0330

    Add aonther way to solve this question

--- Arrays & Strings/238. Product of Array except self/index.js
+++ Arrays & Strings/238. Product of Array except self/index.js
@@ -4,21 +4,36 @@
  */
  
 var productExceptSelf = function (nums) {
-    const n = nums.length;
+    // (1)
     const result = [];
-    
+
     let leftProduct = 1;
     let rightProduct = 1;
-    
-    for (let i = 0; i < n; i++) {
+
+    for (let i=0; i<nums.length; i++) {
         result[i] = leftProduct;
         leftProduct *= nums[i];
     }
-    
-    for (let i = n - 1; i >= 0; i--) {
+
+    for (let i=nums.length - 1; i>=0; i--) {
         result[i] *= rightProduct;
         rightProduct *= nums[i];
     }
-    
+
     return result;
+
+    // (2)
+    // for (let i=0; i<nums.length; i++) {
+    //     let sum = 1;
+
+    //     for (let j=0; i<nums.length; j++) {
+    //         if (i !== j) {
+    //             sum *= nums[j];
+    //         }
+    //     }
+
+    //     ans.push(sum);
+    // }
+
+    // return sum;
 };
