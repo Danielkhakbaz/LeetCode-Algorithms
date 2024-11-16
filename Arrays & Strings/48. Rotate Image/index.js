e057a3c07a35561151511cd2726378d3a53441c3
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-16 13:54:12 +0330

    Add another way to solve this question

--- Arrays & Strings/48. Rotate Image/index.js
+++ Arrays & Strings/48. Rotate Image/index.js
@@ -4,17 +4,31 @@
  */
 
 var rotate = function(matrix) {
-    const len = matrix.length;
+    // (1)
+    // const len = matrix.length;
 
-    for (let i=0; i<len; i++) {
-        for (let j=i+1; j<len; j++) {
+    // for (let i=0; i<len; i++) {
+    //     for (let j=i+1; j<len; j++) {
+    //         [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    //     }
+    // }
+
+    // for (let i=0; i<len; i++) {
+    //     for (let j=0; j<len / 2; j++) {
+    //         [matrix[i][j], matrix[i][len - j - 1]] = [matrix[i][len - j - 1], matrix[i][j]];
+    //     }
+    // }
+
+    // (2)
+    for (let i=0; i<matrix[0].length; i++) {
+        for (let j=i+1; j<matrix[0].length; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
 
-    for (let i=0; i<len; i++) {
-        for (let j=0; j<len / 2; j++) {
-            [matrix[i][j], matrix[i][len - j - 1]] = [matrix[i][len - j - 1], matrix[i][j]];
-        }
+    for (let i=0; i<matrix[0].length; i++) {
+        matrix[i].reverse();
     }
+
+    return matrix;
 };
