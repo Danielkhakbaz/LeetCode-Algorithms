729119149dee7fae7c6711f8472f82a744b3f3be
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-18 16:14:34 +0330

    Add another way to solve the question

--- Hashmaps & Sets/217. Contains Duplicate/index.js
+++ Hashmaps & Sets/217. Contains Duplicate/index.js
@@ -4,7 +4,13 @@
  */
 
 var containsDuplicate = function(nums) {
-  const setNums = new Set(nums);
+    // (1)
+    //   const setNums = new Set(nums);
 
-  return true ? nums.length !== setNums.size : false;
+    //   return true ? nums.length !== setNums.size : false;
+
+    // (2)
+    const set = new Set(nums);
+
+    return set.size === nums.length ? false : true;
 };
