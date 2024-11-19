7b3d067e1c55e4537b2598f1c9175b6a6e4e5320
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-19 14:13:48 +0330

    Add another way to solve this question

--- Hashmaps & Sets/1. Two Sum/index.js
+++ Hashmaps & Sets/1. Two Sum/index.js
@@ -5,23 +5,25 @@
  */
 
 var twoSum = function(nums, target) {
-    for (let i=0; i<nums.length - 1; i++) {
-        for (let j= i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
-    }    
-
-    // let hash = {};
-
-    // for (let key in nums) {
-    //     hash[nums[key]] = key;
-    // }
-
-    // for (let i=0; i<nums.length; i++) {
-    //     if (hash[target - nums[i]] && hash[target - nums[i]] !== i) {
-    //         return [i, hash[target - nums[i]]];
+    // (1)
+    // for (let i=0; i<nums.length - 1; i++) {
+    //     for (let j= i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j];
+    //         }
     //     }
-    // }
+    // }    
+
+    // (2)
+    let hash = {};
+
+    for (let key in nums) {
+        hash[nums[key]] = key;
+    }
+
+    for (let i=0; i<nums.length; i++) {
+        if (hash[target - nums[i]] && parseInt(hash[target - nums[i]]) !== i) {
+            return [i, parseInt(hash[target - nums[i]])];
+        }
+    }
 };
