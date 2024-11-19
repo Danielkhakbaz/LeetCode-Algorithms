a1b795d13d6a8850525ce8dfd55ec93737ed9731
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-19 14:49:49 +0330

    Add another way to solve this question

--- Hashmaps & Sets/49. Group Anagrams/index.js
+++ Hashmaps & Sets/49. Group Anagrams/index.js
@@ -4,23 +4,44 @@
  */
 
 var groupAnagrams = function(strs) {
-    const anagramsMap = new Map();
+    // const anagramsMap = new Map();
     
-    strs.forEach(s => {
-        const count = new Array(26).fill(0);
+    // strs.forEach(s => {
+    //     const count = new Array(26).fill(0);
         
-        for (let c of s) {
-            count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    //     for (let c of s) {
+    //         count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    //     }
+
+        // const key = count.join(',');
+
+    //     if (!anagramsMap.has(key)) {
+    //         anagramsMap.set(key, []);
+    //     }
+        
+        // anagramsMap.get(key).push(s);
+    // });
+    
+    // return Array.from(anagramsMap.values());
+    
+    // (2)
+    let hash = {};
+
+    for (let str of strs) {
+        let array = new Array(26).fill(0);
+
+        for (let ch of str) {
+            array[ch.charCodeAt("0") - "a".charCodeAt("0")]++;
         }
 
-        const key = count.join(',');
-
-        if (!anagramsMap.has(key)) {
-            anagramsMap.set(key, []);
-        }
+        let key = array.join(",");
         
-        anagramsMap.get(key).push(s);
-    });
-    
-    return Array.from(anagramsMap.values());
+        if (hash[key]) {
+            hash[key].push(str);
+        } else {
+            hash[key] = [str];
+        }
+    }
+
+    return Array.from(Object.values(hash))  
 };
