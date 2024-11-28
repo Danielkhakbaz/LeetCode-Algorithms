152520afa9528a43f072ba2089ea34ff31b67fc2
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-28 12:27:45 +0330

    Add another way to solve this question

--- Binary Search/278. First Bad Version/index.js
+++ Binary Search/278. First Bad Version/index.js
@@ -1,6 +1,6 @@
 /**
  * Definition for isBadVersion()
- * 
+ *
  * @param {integer} version number
  * @return {boolean} whether the version is bad
  * isBadVersion = function(version) {
@@ -12,32 +12,49 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
-var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        // for (let i=0; i<=n; i++) {
-        //     if (isBadVersion(i)) {
-        //         return i;
-        //     }
-        // }
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    // for (let i=0; i<=n; i++) {
+    //     if (isBadVersion(i)) {
+    //         return i;
+    //     }
+    // }
 
-        // Too Slow
+    // Too Slow
 
-        let left = 1, right = n;
+    // (1)
+    // let left = 1, right = n;
 
-        while (left < right) {
-            let middle = Math.floor((left + right) / 2);
+    // while (left < right) {
+    //     let middle = Math.floor((left + right) / 2);
 
-            if (isBadVersion(middle)) {
-                right = middle;
-            } else {
-                left = middle + 1;
-            }
-        }
+    //     if (isBadVersion(middle)) {
+    //         right = middle;
+    //     } else {
+    //         left = middle + 1;
+    //     }
+    // }
 
-        return left;
-    };
+    // return left;
+
+    // (2)
+    let left = 0,
+      right = n - 1;
+
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+
+      if (isBadVersion(mid)) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  };
 };
