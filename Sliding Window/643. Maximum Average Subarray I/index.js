feed4f64521153cdef3aee53f6641d9c916d5f7d
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-12-08 11:59:14 +0330

    Add another way to solve this question

--- Sliding Window/643. Maximum Average Subarray I/index.js
+++ Sliding Window/643. Maximum Average Subarray I/index.js
@@ -5,23 +5,43 @@
  */
 
 var findMaxAverage = function(nums, k) {
+    // (1)
+    // let sum = 0;
+    // let n = nums.length;
+
+    // for (let i=0; i<k; i++) {
+    //     sum += nums[i];
+    // }
+
+    // let average = sum / k;
+
+    // for (let i=k; i<n; i++) {
+    //     sum += nums[i];
+    //     sum -= nums[i - k];
+
+    //     current_average = sum / k;
+
+    //     average = Math.max(average, current_average);
+    // }
+
+    // return average;
+
+    // (2)
     let sum = 0;
-    let n = nums.length;
+    let max = -Infinity;
 
     for (let i=0; i<k; i++) {
         sum += nums[i];
     }
 
-    let average = sum / k;
-
-    for (let i=k; i<n; i++) {
-        sum += nums[i];
+    max = sum;
+    
+    for (let i=k; i<nums.length; i++) {
+        sum += nums[i]; 
         sum -= nums[i - k];
 
-        current_average = sum / k;
-
-        average = Math.max(average, current_average);
+        max = Math.max(max, sum);
     }
 
-    return average;
+    return max / k;
 };
