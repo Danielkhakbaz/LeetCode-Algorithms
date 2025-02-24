9379828530da1031cab00d844c37ae6ca429b2e8
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2025-02-24 12:13:39 +0330

    Add another and a better way to solve this question

--- Hashmaps & Sets/1189. Maximum Number of Balloons/index.py
+++ Hashmaps & Sets/1189. Maximum Number of Balloons/index.py
@@ -1,22 +1,35 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        balloon = "balloon"
+        # (1)
+        # balloon = "balloon"
 
-        hash = {}
+        # hash = {}
 
-        for ch in text:
-            if ch in balloon:
-                hash[ch] = hash.get(ch, 0) + 1
+        # for ch in text:
+        #     if ch in balloon:
+        #         hash[ch] = hash.get(ch, 0) + 1
         
-        if "l" in hash:
-            hash["l"] = hash["l"] // 2
-        if "o" in hash:
-            hash["o"] = hash["o"] // 2
+        # if "l" in hash:
+        #     hash["l"] = hash["l"] // 2
+        # if "o" in hash:
+        #     hash["o"] = hash["o"] // 2
 
-        for ch in balloon:
-            if ch not in hash:
+        # for ch in balloon:
+        #     if ch not in hash:
+        #         return 0
+
+        # print(hash)
+
+        # return min(hash.values())
+    
+        allowed_characters = {"b", "a", "l", "o", "n"}
+        hash = Counter(ch for ch in text if ch in allowed_characters)
+
+        hash["l"] = hash["l"] // 2
+        hash["o"] = hash["o"] // 2
+
+        for ch in allowed_characters:
+            if ch not in text:
                 return 0
-
-        print(hash)
 
         return min(hash.values())
